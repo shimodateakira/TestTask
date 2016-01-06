831682b693530e7f53739a041f3a9129f90a0f9f
shimodateakira <shimodateakira@i.softbank.jp>
2016-01-06 19:36:00 +0900

    task.AsyncStateの動作を確認するコードを追加。

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestTask
@@ -10,10 +9,65 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int MaxCount = 10;
+            int previous = -1;
+            int current = 0;
+            State state = new State() { Count = MaxCount, Current = 0 };
+
+            // 呼び出し方法 その1
+            // Taskのインスタンスを作ってからStartする。
+            //
+            //var task = new Task<int>(o => CountUpMethod((State)o), state);
+            //task.Start();
+
+            // 呼び出し方法 その2
+            // Taskのインスタンスを作ってからRunするが、
+            // stateはTaskのインスタンスに渡していない為、
+            // task.AsyncStateがnullになり、NullReferenceExceptionが発生する。
+            //
+            //var task = Task.Run(() => CountUpMethod(state));
+
+            // 呼び出し方法 その3
+            // TaskのFactoryのStartNewでインスタンスの作成とスタートを同時に実行する。
+            // また、stateをTaskのインスタンスに渡している(インスタンス内にstateが保存される) 
+            var task = Task.Factory.StartNew(o => CountUpMethod((State)o), state);
+
+            while (current < MaxCount)
+            {
+                current = ((State)task.AsyncState).Current;
+                if(current != previous )
+                {
+                    Console.Write($"{current} ");
+                    previous = current;
+                }
+#if DEBUG
+                Debug.WriteLine($"{current}");
+#endif
+            }
 
             Console.WriteLine("\n\nPush any key!");
             Console.ReadKey();
         }
+
+        static int CountUpMethod(State state)
+        {
+            int count = 0;
+
+            for (int i = 0; i < state.Count; i++)
+            {
+                count++;
+                state.Current = count;
+                Thread.Sleep(1000); // 1秒
+            }
+
+            return count;
+        }
+        
+    }
+
+    class State
+    {
+        public int Current { get; set; }
+        public int Count { get; set; }
     }
 }
