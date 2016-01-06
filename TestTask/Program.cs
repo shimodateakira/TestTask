2b74bbf3af52fb10e24853374127f76fb4325899
shimodateakira <shimodateakira@i.softbank.jp>
2016-01-06 17:50:33 +0900

    初期登録。

---
+++ TestTask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\nPush any key!");
+            Console.ReadKey();
+        }
+    }
+}
